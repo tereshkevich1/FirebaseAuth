3a8255c6f0a01fc2d2a880e8aeb977d74486a547
tereshkevich1 <tereshkevich.cergey@mail.ru>
2025-03-03 18:34:56 +0300

    feat: implement splashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.json)
 
+    implementation (libs.androidx.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.firebaseauth.presentation.SplashViewModel
+import com.example.firebaseauth.ui.theme.FirebaseAuthTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {}
+
+        splashScreen.setKeepOnScreenCondition { viewModel.isLoading.value }
+        setContent {
+            FirebaseAuthTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Main Activity")
+                    }
+                }
+            }
+        }
     }
 }
